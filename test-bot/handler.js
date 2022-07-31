d7764c244e57a6e8129d665fd12602a88de5b8d8
seung <skyhs3507@likelion.org>
2022-08-01 00:46:31 +0900

    Add check genesis logic

--- test-bot/handler.js
+++ test-bot/handler.js
@@ -29,78 +29,144 @@ const initialize = async (account) => {
   }
 }
 
+const sendErrorMeg = async (response) => {
+  const status = {}
+  status["error_name"] = response.error.name;
+  status["error_cause"] = response.error.cause.name;
+  status["error_info"] = response.error.cause.info;
+  const message = `${status["error_name"]} 에러가 발생했습니다. : ${status["error_cause"]} / ${status["error_info"]}`;
+  // console.log("node Status :", status);
+  await axios.post(WEBHOOK_URL, {
+      "attachments": [
+          {
+              "fallback": "요청이 실패했습니다.",
+              "color": "#2eb886",
+              "pretext": "노드 에러",
+              "text": message,
+              "footer_icon": "https://platform.slack-edge.com/img/default_application_icon.png",
+          }
+      ]
+    })
+}
+
+
+const checkGenesisStatus = async (near) => {
+  const response = await near.connection.provider.experimental_protocolConfig({ sync_checkpoint: 'genesis' });
+  //console.log("genesis response : ", response);
+  const status = {}
+  if (response.protocol_version){
+    status["genesis_time"] = response.genesis_time.slice(0,19);
+    console.log("genesis time : ", status["genesis_time"]);
+    const hardforkMsg = response.genesis_height > process.env.BLOCK_HEIGHT ? "Genesis Height가 달라져 하드포크가 필요합니다." : "";
+    console.log("hardfork : ", hardforkMsg);
+    await axios.post(WEBHOOK_URL, {
+        "attachments": [
+            {
+                "fallback": "요청이 실패했습니다.",
+                "color": "#2eb886",
+                "text": `Genesis Block Time : ${status["genesis_time"]} | ${hardforkMsg}`
+            }
+        ]
+    })
+  }
+  if (response.error){
+    await sendErrorMeg(response);
+    }
+  return status;
+}
+
 const checkNodeStatus = async (near) => {
   const response = await near.connection.provider.status();
   const status = {}
   if (response.sync_info){
-      const protocol_version_gap = response.latest_protocol_version > response.protocol_version;        
-      status["protocol_version_gap"] = protocol_version_gap;
-      await axios.post(WEBHOOK_URL, {
+    status["protocol_version_gapped"] = response.latest_protocol_version > response.protocol_version;
+    const message = status["protocol_version_gapped"] ? `현재 최신 프로토콜 버전이 아닙니다.` : `현재 최신 프로토콜 버전입니다.`;
+    // console.log("node Status :", status);
+    await axios.post(WEBHOOK_URL, {
         "attachments": [
             {
-                "fallback": "요청 실패 시 보낼 메시지",
+                "fallback": "요청이 실패했습니다.",
                 "color": "#2eb886",
-                "pretext": "Lambda Bot",
-                "title": "Node Status",
-                "text": `프로토콜 버전 차이 : ${protocol_version_gap}`,
+                "pretext": "프로토콜 버전 관리",
+                "text": message,
+            },
+            {
+                "fallback": "요청이 실패했습니다.",
+                "color": "#2eb886",
+                "text": `최신 Block Time : ${response.sync_info.latest_block_time}`
             }
         ]
-      });
+    })
   }
   if (response.error){
-      status["error_name"] = response.error.name;
-      status["error_cause"] = response.error.cause.name;
-      status["error_info"] = response.error.cause.info;
-      await axios.post(WEBHOOK_URL, {
-        "attachments": [
-            {
-                "fallback": "요청 실패 시 보낼 메시지",
-                "color": "#2eb886",
-                "pretext": "Lambda Bot",
-                "title": "Node Status",
-                "text": `${status["error_name"]} Error : ${status["error_info"]}`,
-            }
-        ]
-      });
-  }
-  
+    await sendErrorMeg(response);
+    }
   return status;
 }
 
-const checkValidatorStatus = async (near, account) => {
+const checkValidatorStatus = async (near, validator_id) => {
   const response = await near.connection.provider.validators(null);
-  const selected_validator = response.current_validators.filter((validator) => {
-      return validator.account_id.includes(account);
-  });
-  if(selected_validator.length > 0 ){
-    const upTime = selected_validator[0].num_expected_blocks / selected_validator[0].num_produced_blocks * 100;
-    console.log("Uptime :", upTime);
-    await axios.post(WEBHOOK_URL, {
-        "attachments": [
-            {
-                "fallback": "요청 실패 시 보낼 메시지",
-                "color": "#2eb886",
-                "title": "Node Status",
-                "text": `UpTime : ${upTime}%`,
-            }
-        ]
-      });
-      return selected_validator[0].is_slashed;
+  const id = validator_id+".factory.shardnet.near";
+  const status = {};
+  if(response.current_validators){
+      const selected_validators = response.current_validators.filter((validator) => {
+          return validator.account_id.includes(id);
+      })
+      console.log("selected_validators : ", selected_validators[0]);
+      console.log("Uptime calculation : ", ((selected_validators[0].num_produced_blocks / selected_validators[0].num_expected_blocks) + (selected_validators[0].num_produced_chunks / selected_validators[0].num_expected_chunks)) / 2)
+      if(selected_validators.length === 0){
+          status["validator_joined"] = false;
+          const next_validators = response.next_validators.filter(validator => validator.account_id.includes(id));
+          if (next_validators.length === 0){
+              status["validator_expected_join"] = false;
+          }
+          else{
+              status["validator_expected_join"] = true;
+          }
+      }
+      else{
+          status["validator_joined"] = true;
+          status["validator_slashed"] = selected_validators[0].is_slashed;
+          status["validator_produced_chunks"] = selected_validators[0].num_produced_chunks;
+          status["uptime_ratio"] =  ((selected_validators[0].num_produced_blocks / selected_validators[0].num_expected_blocks) + (selected_validators[0].num_produced_chunks / selected_validators[0].num_expected_chunks)) / 2 * 100;
+      }
+      const status_message = status["validator_joined"] ? "현재 Validator로 참여 중입니다." : "현재 Validator로 참여하고 있지않습니다.";
+      const slashed_message = status["validator_slashed"] ? "Kicked Out 되었습니다" : "Kicked Out 상태가 아닙니다."
+      const produced_chunks_message = `Produced Chunks : ${status["validator_produced_chunks"]}`; 
+      const upTime_message= status["uptime_ratio"];
+      await axios.post(WEBHOOK_URL, {
+          "attachments": [
+              {
+                  "fallback": "Validator join 조회 요청이 실패했습니다.",
+                  "color": "#2eb886",
+                  "pretext": "Validator 상태",
+                  "text": `${status_message} |  ${produced_chunks_message} | Uptime Ratio : ${upTime_message.toFixed(2)}%`,
+              },
+              {
+                  "fallback": "Validator join 조회 요청이 실패했습니다.",
+                  "color": "#2eb886",
+                  "text": slashed_message,
+              },
+          ]
+      })
+      if (!status["validator_joined"]){
+          const next_join_message = status["validator_expected_join"] ? "다음 Validator로 참여 예정입니다." : "다음 Validator로 참여하지 않습니다.";
+          await axios.post(WEBHOOK_URL, {
+              "attachments": [
+                  {
+                      "fallback": "Validator join 조회 요청이 실패했습니다.",
+                      "color": "#2eb886",
+                      "text": next_join_message,
+                      "footer_icon": "https://platform.slack-edge.com/img/default_application_icon.png",
+                  },
+              ]
+          })
+      }
   }
   if (response.error){
-    await axios.post(WEBHOOK_URL, {
-      "attachments": [
-          {
-              "fallback": "요청 실패 시 보낼 메시지",
-              "color": "#2eb886",
-              "pretext": "Lambda Bot",
-              "title": "Node Status",
-              "text": `${response.error.name} Error : ${response.error.cause.info}`,
-          }
-      ]
-    });
-}
-  return null;
+    await sendErrorMeg(response);
+  }
+  return status;
 }
 
 module.exports.bot_test = async (event) => {
@@ -109,7 +175,8 @@ module.exports.bot_test = async (event) => {
     const near = await initialize(accountId);
     console.log("initialize ===");
     await checkNodeStatus(near);
-    await checkValidatorStatus(near, `${process.env.ACCOUNT}.factory.shardnet.near`);
+    await checkValidatorStatus(near, process.env.ACCOUNT);
+    await checkGenesisStatus(near);
     return {
       statusCode: 200,
       body: JSON.stringify(
@@ -124,7 +191,7 @@ module.exports.bot_test = async (event) => {
     return {
       statusCode: 500,
       body : JSON.stringify(
-        {message : e},
+        {message : e.message},
         null,
         2
       )
